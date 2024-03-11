f3a4587d8df87eef585136edf278c9169bdea07a
aghyad <aghyad>
2024-03-11 11:39:35 +0400

    addedd new files

--- src/components/Stripe.jsx
+++ src/components/Stripe.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-
 import CheckoutForm from "./CheckoutForm";
 
 
