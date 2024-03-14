46f4456b0e9c9fcc82b6582378341b6383ed90fd
aghyad <aghyad>
2024-03-14 10:30:34 +0400

    lazy fazal

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -5,7 +5,7 @@ import {
   useElements
 } from "@stripe/react-stripe-js";
 
-export default function CheckoutForm() {
+export default function CheckoutForm({totalAmount}) {
   const stripe = useStripe();
   const elements = useElements();
 
@@ -87,7 +87,7 @@ export default function CheckoutForm() {
       <PaymentElement id="payment-element" options={paymentElementOptions} />
       <button disabled={isLoading || !stripe || !elements} id="submit">
         <span id="button-text">
-          {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
+          {isLoading ? <div className="spinner" id="spinner"></div> : `Pay ${totalAmount} AED`}
         </span>
       </button>
       {/* Show any error or success messages */}
